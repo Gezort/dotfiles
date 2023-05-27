8aaca3da884e24bc1c7b11cbb956645f79916c76
Gezort <miron_55@mail.ru>
2023-05-27 10:00:20 +0300

    upd telescope cfg

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -37,18 +37,33 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
+-- Bindings
+
+local tbuiltin = require('telescope.builtin')
+local get_file_search_dir = function ()
+  local cur_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+  if string.find(cur_dir, "datacamp") then
+    return string.match(cur_dir, '.*/datacamp/')
+  end
+
+  return cur_dir .. "/.."
+end
+
+local search_files = function ()
+  return tbuiltin.find_files({cwd=get_file_search_dir()})
+end
+
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>?', tbuiltin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', tbuiltin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
-  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+  tbuiltin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
     winblend = 10,
     previewer = false,
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
-vim.keymap.set('n', '<leader>sw', require('telescope.builtin').live_grep, { desc = '[S]earch word by [G]rep' })
-
-vim.keymap.set('n', '<leader>ch', require('telescope.builtin').command_history, { desc = 'Search in [C]ommand [H]istory' })
+vim.keymap.set('n', '<leader>sf', search_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sw', tbuiltin.live_grep, { desc = '[S]earch word by [G]rep' })
+vim.keymap.set('n', '<leader>sch', tbuiltin.command_history, { desc = '[S]earch in [C]ommand [H]istory' })
