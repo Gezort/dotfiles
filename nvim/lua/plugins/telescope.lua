e1c571600d86ccec860050045bf788bd86fb26a0
Gezort <miron_55@mail.ru>
2023-05-27 09:50:03 +0300

    upd telescope cfg

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -37,9 +37,33 @@ require('telescope').setup {
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
 
+-- Bindings
+
+local tbuiltin = require('telescope.builtin')
+local get_file_search_dir = function ()
+  local cur_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+  local depth = 1
+  for dir in vim.fs.parents(cur_dir) do
+    if dir == vim.loop.os_homedir() then
+      break
+    else
+      depth = depth + 1
+    end
+  end
+  if depth < 4 then
+    return cur_dir
+  else
+    return cur_dir .. string.rep("/..", depth - 3)
+  end
+end
+
+local search_files = function ()
+  return tbuiltin.find_files({cwd=get_file_search_dir()})
+end
+
 -- See `:help telescope.builtin`
-vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
+vim.keymap.set('n', '<leader>?', tbuiltin.oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', tbuiltin.buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -48,7 +72,7 @@ vim.keymap.set('n', '<leader>/', function()
   })
 end, { desc = '[/] Fuzzily search in current buffer]' })
 
-vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sf', search_files, { desc = '[S]earch [F]iles' })
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').live_grep, { desc = '[S]earch word by [G]rep' })
 
-vim.keymap.set('n', '<leader>ch', require('telescope.builtin').command_history, { desc = 'Search in [C]ommand [H]istory' })
+vim.keymap.set('n', '<leader>sch', require('telescope.builtin').command_history, { desc = '[S]earch in [C]ommand [H]istory' })
